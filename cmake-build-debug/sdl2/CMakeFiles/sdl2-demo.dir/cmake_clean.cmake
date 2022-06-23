51f65899030152072af7b82057d9913fc23574ce
李智骁 <hobby.li@tuya.com>
2022-06-23 21:31:46 +0800

    ADD

---
+++ cmake-build-debug/sdl2/CMakeFiles/sdl2-demo.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/sdl2-demo.dir/main.cpp.o"
+  "sdl2-demo"
+  "sdl2-demo.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/sdl2-demo.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
